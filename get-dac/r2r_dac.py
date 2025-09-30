fc410583a2d22e8e7f966255b90af00e8485d191
Veronika <veronikamuzovatkina1@gmail.com>
2025-09-30 11:22:49 +0300

    теперь первая прога работает

---
+++ get-dac/r2r_dac.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+dac_bits = [16, 20, 21, 25, 26, 17, 27, 22]
+GPIO.setup(dac_bits, GPIO.OUT)
+GPIO.output(dac_bits, 0)
+class R2R_DAC:
+    def __init__(self, gpio_bits, dynamic_range, verbose = False):
+        self.gpio_bits = gpio_bits
+        self.dynamic_range = dynamic_range
+        self
